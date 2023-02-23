dee62a60e4985a675d2ca19b05a73824f57e78e2
Alina-kio <alinaolkim@gmail.com>
2023-02-24 02:20:59 +0600

    2 month, hw 5

---
+++ app/src/main/java/com/example/alina_kim_m2_hw5/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.alina_kim_m2_hw5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
